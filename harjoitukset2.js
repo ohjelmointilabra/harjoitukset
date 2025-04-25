b95defba65285af3d7a10a4f739613c8fd83084e
Martinonyte Dovile <71691245+dovile-mart@users.noreply.github.com>
2025-04-25 15:45:59 +0300

    Array added

---
+++ harjoitukset2.js
@@ -0,0 +1,23 @@
+function tulostaTaulukko(){
+    let numerot1 = [12, 34, 54, 2, 16];
+    numerot1 = [15, 24, 3, 45]
+    
+    document.getElementById("taulukko-vastaus").innerHTML = "Numerot 1: " + numerot1;
+    document.getElementById("taulukko-vastaus").innerHTML = "Numerot 1: " + numerot1[2];
+    
+    /*let numero3 = numerot1.at(-2);
+    document.getElementById("taulukko-vastaus").innerHTML = numero3*/
+
+    const numerot2 = [2, 44, 67, 32, 1];
+    numerot2 = [53, 34, 55, 8]
+    
+    document.getElementById("taulukko-vastaus").innerHTML = "Numerot 2: " + numerot2;
+}
+
+// 10. Mikä on taulukon 'numerot1' pituus?
+
+// 11. Millä numerolla viittaat listan ensimmäiseen alkioon?
+
+// 12. Millä metodilla lisäät uuden alkion taulukon loppuun?
+
+// BONUS 2. Miksi taulukkoa 'numerot2' ei tulosteta? Miten korjaat virheen? 
